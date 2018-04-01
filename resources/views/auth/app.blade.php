e44a46b82b9c1d6c7832cace8410e986a80aa586
ihankhong <hank790315@gmail.com>
2018-04-01 21:16:18 +0800

    edit

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -27,6 +27,14 @@
     <link rel="stylesheet" href="{{ mix('css/jquery-ui.css') }}">
     <link rel="stylesheet" href="{{ mix('css/pages.css') }}" class="main-stylesheet">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script type="text/javascript">
+    window.onload = function()
+    {
+      // fix for windows 8
+      if (navigator.appVersion.indexOf("Windows NT 6.2") != -1)
+        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="{{ asset('css/windows.chrome.fix.css') }}" />'
+    }
+    </script>
 </head>
 <body class="fixed-header">
     <div class="login-wrapper">
